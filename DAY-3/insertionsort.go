f1b50b619255c5b9cd3d22ee2cb6e13fe1f7f4a8
HerPerception <offorbuzor1@gmail.com>
2026-09-14 21:29:01 +0100

    Modify Insertion Sort to handle larger integer array
    Updated the insertion sort implementation to handle a larger array with different values and improved the insertion logic.

--- DAY-3/insertionsort.go
+++ DAY-3/insertionsort.go
@@ -10,23 +10,37 @@ package main
 import "fmt"
 
 func InsertionSort() {
-	myArray := []int64{64, 34, 25, 12, 22, 11, 90, 5, 100, 89, 78}
+	myArray := []int{64, 34, 25, 12, 22, 11, 90, 5, 3, 50, 100, 300, 200, 2}
 	n := len(myArray)
 	for i := 1; i < n; i++ {
 		insertIndex := i
 		currVal := myArray[i]
-		fmt.Println("current value", currVal)
-		myArray = append(myArray[:i], myArray[i+1:]...)
+		fmt.Println(currVal)
 		for j := i - 1; j >= 0; j-- {
 			if myArray[j] > currVal {
 				insertIndex = j
-				fmt.Println("j index", myArray[j])
+				myArray[j+1] = myArray[j]
+				fmt.Println(myArray)
+				myArray[insertIndex] = currVal
+				fmt.Println(myArray)
 			} else {
 				break
 			}
 		}
-		myArray = append(myArray[:insertIndex], append([]int64{currVal}, myArray[insertIndex:]...)...)
 		fmt.Println(myArray)
 	}
 	fmt.Println(myArray)
 }
+
+/*============================================================================================
+We break out of the inner loop because there's no need comparing values when the correct place
+for the currrent value is found.
+TIME COMPLEXITY
+Insertion Sort sorts an array of n values.
+On average, each value must be compared to about n\2 other values to find the correct place 
+to insert it.
+Insertion Sort must run the loop to insert a value in its correct place approximately n times.
+Time complexity is O(n²)
+For Insertion Sort, there is a big difference between best, average and worst case scenarios. 
+==============================================================================================*/
+
